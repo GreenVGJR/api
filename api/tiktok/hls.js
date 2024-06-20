76eef3339c6129f5337c279d7bbf8bc2ed1dbc15
GreenVGJR <greenvgjrgaming@gmail.com>
2024-06-20 22:55:03 +0700

    Update hls.js

--- api/tiktok/hls.js
+++ api/tiktok/hls.js
@@ -25,13 +25,13 @@ export default async function handler(req, res) {
     const { url, watermark } = req.query;
 
     if (!url || typeof url !== 'string') {
-        if(!url.includes('tiktok.com') && typeof watermark !== 'boolean') {
+        if(!url.includes('tiktok.com') && typeof watermark != 'boolean') {
         return res.status(400).json({ error: 'Invalid or missing URL parameter' });
        }
     }
 
     let wm;
-    if(!watermark) {
+    if(watermark == 'true') {
       wm = '"downloadAddr":"';
     }
     else {

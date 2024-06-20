9434ff56f12c9c827dc3a7b59636e3baec83a1c6
GreenVGJR <greenvgjrgaming@gmail.com>
2024-06-20 18:30:35 +0700

    api

--- api/index.js
+++ api/index.js
@@ -1,4 +1,20 @@
-// api/hello.js
-export default function handler(req, res) {
-  res.status(200).json({ message: 'Hello, World!' });
+// api/getCookieTiktok.js
+import axios from 'axios';
+
+export default async function handler(req, res) {
+    const url = 'https://www.tiktok.com';
+    const headers = {
+        'User-Agent': req.headers['user-agent'] || 'undici',
+        'Referer': 'https://www.tiktok.com/'
+    };
+
+    try {
+        const response = await axios.get(url, { headers: headers });
+        const setCookieHeader = response.headers['set-cookie'];
+        const setCookieString = Array.isArray(setCookieHeader) ? setCookieHeader.join('; ') : setCookieHeader;
+
+        res.status(200).json({ cookies: setCookieString });
+    } catch (error) {
+        res.status(403).json({ error: 'Failed to fetch cookies' });
+    }
 }

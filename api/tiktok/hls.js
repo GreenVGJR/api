bb3e56c0013204d7490e43286fc0ffc69a87aca3
GreenVGJR <greenvgjrgaming@gmail.com>
2024-06-20 19:18:23 +0700

    Update hls.js

--- api/tiktok/hls.js
+++ api/tiktok/hls.js
@@ -1,15 +1,29 @@
 // api/tiktok/hls.js
+import axios from 'axios/dist/node/axios.cjs';
 
 export default async function handler(req, res) {
    const { url } = req.query;
 
-   if (!url || typeof url !== 'string') {
+   if (!url && typeof url !== 'string' && url.includes('tiktok.com')) {
        return res.status(400).json({ error: 'Invalid or missing URL parameter' });
+   }
+
+   if(url.includes("vt.tiktok.com") || url.includes("vm.tiktok.com")) {
+       const realurl = url;
+       const headers = {
+        'User-Agent': req.headers['user-agent'] || 'undici',
+        'Referer': 'https://www.tiktok.com/'
+    };
+    const response = await axios.get(realurl, { headers: headers });
+
+    const data = response.data;
+    const playAddrPart = data.split('"playAddr":"')[1];
+    const playAddr = playAddrPart.split('"')[0];
    }
 
    try {
        // Process the URL parameter as needed (e.g., fetch data)
-       const responseData = { message: 'Data fetched for URL: ' + url };
+       const responseData = { message: 'Data fetched for URL: ' + playAddr };
 
        // Return a JSON response with the processed data
        res.status(200).json(responseData);

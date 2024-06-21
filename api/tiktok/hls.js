8b5bdfe65230dcb121f46ea8bf22798b2ac2d336
GreenVGJR <greenvgjrgaming@gmail.com>
2024-06-21 10:46:28 +0700

    Update hls.js

--- api/tiktok/hls.js
+++ api/tiktok/hls.js
@@ -38,6 +38,8 @@ export default async function handler(req, res) {
 
     // Check if request count exceeds maximum allowed
     if (requestCount >= maxRequestsPerCooldown) {
+        lastRequestTime = 0
+        requestCount = 0;
         return res.status(429).end();
     }
 

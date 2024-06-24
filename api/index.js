3df486bf880061772f97d7e0c828b0c69c694bb3
GreenVGJR <greenvgjrgaming@gmail.com>
2024-06-24 08:42:08 +0700

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,35 @@
+const cooldownTime = 15 * 1000;
+let lastRequestTime = Date.now();
+
+let requestCount = 0;
+const maxRequestsPerCooldown = 5;
+
+export default async function handler(req, res) {
+   const currentTime = Date.now();
+
+   // Reset request count if cooldown period has elapsed
+   if (currentTime - lastRequestTime > cooldownTime) {
+       lastRequestTime = currentTime;
+       requestCount = 0;
+   }
+
+   // Check if request count exceeds maximum allowed
+   if (requestCount >= maxRequestsPerCooldown) {
+       return res.status(429).end();
+   }
+
+   // Increment request count
+   requestCount++;
+
+   res.status(200).json({ 
+      status: true,
+      list: [{
+         downloader: [{
+            tiktok: ["/tiktok", "/tiktok/hls.js"],
+            soundcloud: "/sc",
+            twitter: "/tw",
+            facebook: "/fb"
+         }]
+      }]
+   })
+}

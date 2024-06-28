5fb2c5f80dc9e54bfbfb49b7f4e27c628dedac39
GreenVGJR <greenvgjrgaming@gmail.com>
2024-06-28 22:57:52 +0700

    Update hls.js

--- api/tiktok/hls.js
+++ api/tiktok/hls.js
@@ -69,28 +69,9 @@ export default async function handler(req, res) {
         // Decode URI component
         playAddr = decodeURIComponent(playAddr);
 
-        // Stream the video from TikTok to the client
-        const videoResponse = await axios.get(playAddr, { headers, responseType: 'stream' });
-
-        // Set appropriate headers for video streaming
-        if (videoResponse.headers['content-length'] > 4500000) {
-         res.status(500).json({ 
-            status: false,
-            error: 'Vercel Errors: Video size exceeded limit.',
-            payload_limit: '4.5 MB',
-            video_size: Math.floor(videoResponse.headers['content-length'] / 1000 / 1000 * 10) / 10 + ' MB',
-            data: [{
-                hls: `${playAddr}`,
-                cookie: `${token}`
-            }],
-            alternative: "https://tikcdn.io/ssstik/" + url.split('/')[5]
-            });
-         }
-        else {
         const cacheDuration = 60 * 60 * 24 * 1; // 1 days in seconds
         
         res.setHeader('Content-Type', 'application/json');
-        res.setHeader('Content-Length', videoResponse.headers['content-length']);
         res.setHeader('Cache-Control', `public, max-age=${cacheDuration}, immutable, preload`);
         res.setHeader('Cache-Cookie', token);
         if (!audio) {
@@ -106,7 +87,6 @@ export default async function handler(req, res) {
             status: 'Temporary Redirect',
             location: playAddr
         }, null, 2)); // Pretty-print the response 
-        }
     }
      catch (error) {
         if (axios.isAxiosError(error)) {

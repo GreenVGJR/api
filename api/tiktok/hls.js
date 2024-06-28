4f5dfbadfd21d9bb16e6a8610315cb39bc9b810b
GreenVGJR <greenvgjrgaming@gmail.com>
2024-06-28 23:07:02 +0700

    Update hls.js

--- api/tiktok/hls.js
+++ api/tiktok/hls.js
@@ -69,23 +69,40 @@ export default async function handler(req, res) {
         // Decode URI component
         playAddr = decodeURIComponent(playAddr);
 
+        // Stream the video from TikTok to the client
+        const videoResponse = await axios.get(playAddr, { headers, responseType: 'stream' });
+
+        // Set appropriate headers for video streaming
+        if (videoResponse.headers['content-length'] > 4500000) {
+         res.status(500).json({ 
+            status: false,
+            error: 'Vercel Errors: Video size exceeded limit.',
+            payload_limit: '4.5 MB',
+            video_size: Math.floor(videoResponse.headers['content-length'] / 1000 / 1000 * 10) / 10 + ' MB',
+            data: [{
+                hls: `${playAddr}`,
+                cookie: `${token}`
+            }],
+            alternative: "https://tikcdn.io/ssstik/" + url.split('/')[5]
+            });
+         }
+        else {
         const cacheDuration = 60 * 60 * 24 * 1; // 1 days in seconds
-        
-        res.setHeader('Content-Type', 'application/json');
-        res.setHeader('Cache-Control', `public, max-age=${cacheDuration}, immutable, preload`);
-        res.setHeader('Set-Cookie', token);
-        if (!audio) {
-            res.setHeader('video', playAddr);
-        }
-        if (audio === 'true') {
-            res.setHeader('audio', playAddr);
+
+         res.writeHead(200, { 
+            'Content-Type': audio == 'true' ? 'audio/aac' : 'video/mp4',
+            'Content-Length': videoResponse.headers['content-length'],
+            'Cache-Control': `public, max-age=${cacheDuration}, immutable, prefetch`, // HTTP caching header
+            'Cache-Cookie': token, // Use the retrieved token as a cookie
+            'video': !audio ? `${playAddr}` : null,
+            'audio': audio == 'true' ? `${playAddr}` : null
+        });
+
+        // Pipe the video stream to the client's response
+        videoResponse.data.pipe(res);
         }
 
-        // Temporary redirect to the final URL
-        res.setHeader('Location', playAddr);
-        res.status(307).end();
-    }
-     catch (error) {
+    } catch (error) {
         if (axios.isAxiosError(error)) {
             // Axios error handling
             if (error.response) {
@@ -104,7 +121,6 @@ export default async function handler(req, res) {
                 res.status(500).json({ status: false, error: 'Error setting up request' });
             }
         } else {
-            console.error(error);
             res.status(500).end();
         }
     }

a66fbc75baa4f5fe208b90d52ca9641be2166fb5
GreenVGJR <greenvgjrgaming@gmail.com>
2024-06-28 22:24:41 +0700

    Update hls.js

--- api/sc/hls.js
+++ api/sc/hls.js
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
    catch (error) {
    res.status(500).json({
     status: false,
-    message: error.response.data
+    message: error.response.status
    });
    }
 }

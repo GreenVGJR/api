82b1a6d22d197e400a0c1c0f63577ee4dace7605
GreenVGJR <greenvgjrgaming@gmail.com>
2025-02-21 08:35:16 +0700

    hi

---
+++ page/index.php
@@ -0,0 +1,54 @@
+<!--
+* Created with https://www.css-gradient.com
+* Gradient link: https://www.css-gradient.com/?c1=042f47&c2=917eca&gt=l&gd=dtr
+-->
+
+<?php
+http_response_code(503);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Maintenance</title>
+
+   <!-- Poppins Fonts -->
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+   
+</head>
+<body>
+   <p class="text">Maintenance</p>
+</body>
+<style>
+   * {
+      padding: 0;
+      margin: 0;
+   }
+
+   html {
+      background-color: black;
+   }
+
+   body {
+      width: 100%;
+      height: 100vh;
+      background: linear-gradient(225deg, rgba(4, 47, 71, 1.0), rgba(145, 126, 202, 1.0));
+   }
+
+   .text {
+      font-family: 'Poppins', serif;
+      position: absolute;
+      color: rgb(182, 145, 255);
+      bottom: 0;
+      margin: 3rem;
+      font-size: 2rem;
+      font-weight: bold;
+      text-shadow: 0 3px 5px rgba(0, 0, 0, 0.3);
+   }
+
+</style>
+</html>

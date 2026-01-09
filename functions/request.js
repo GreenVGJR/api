940bc21bed59c2799256dd18a65e8ec365b5cf32
GreenVGJR <greenvgjrgaming@gmail.com>
2026-01-09 11:25:37 +0700

    test

--- functions/request.js
+++ functions/request.js
@@ -395,7 +395,7 @@ exports.Genius = async function Genius(que) {
                 ...commonHeaders
             }
         });
-        console.log(pull.body.text());
+        console.log(await pull.body.text());
 
         const res = await pull.body.json();
         return res?.response?.sections?.[0]?.hits?.map(a => a?.result) || res;

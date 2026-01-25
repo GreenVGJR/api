32aefc5d430306d1547f9f2a686d119893afffff
GreenVGJR <greenvgjrgaming@gmail.com>
2026-01-26 02:12:39 +0700

    shibal

--- functions/request.ts
+++ functions/request.ts
@@ -2252,14 +2252,12 @@ export const instagramUser = async function instagramUser(que: string) {
     if(!que) return null;
 
     try {
-        const testreq = await request(`https://www.instagram.com/${que}`, {
+        const testreq = await request(`https://www.instagram.com/${que}/embed`, {
             headers: {
                 ...commonHeaders,
                 'User-Agent': 'Mozilla/5.0 (compatible; Discordbot/2.1; +https://discordapp.com)'
             }
         });
-
-        console.log(testreq);
 
         if(testreq.statusCode === 302) {
             return {
@@ -2268,7 +2266,7 @@ export const instagramUser = async function instagramUser(que: string) {
         }
 
         const resreq = await testreq.body.text();
-        const profile_id = resreq.split('"profile_id":"')[1]?.split('"')?.[0];
+        const profile_id = resreq.split('owner-id="')[1]?.split('"')?.[0];
 
         if(!profile_id) {
             return {
